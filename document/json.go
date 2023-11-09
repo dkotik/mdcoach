19cc4a767211844772043a656371aaacc6aa84d4
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2023-11-09 18:59:44 +0100

    Fix single and double quote encoding

--- document/json.go
+++ document/json.go
@@ -7,6 +7,8 @@ import (
 
 const escapedCharsJSON = "&'<>\"\r\n"
 
+// WriteEscapedJSON writes a string a JSON value with HTML escape codes applied. Single and double quotes are escaped twice. This preserves JSON objects embedded into HTML element body.
+//
 // code borrowed from `html` standard library package, simplified
 func WriteEscapedJSON(w io.Writer, s string) error {
 	i := strings.IndexAny(s, escapedCharsJSON)
@@ -20,19 +22,18 @@ func WriteEscapedJSON(w io.Writer, s string) error {
 			esc = "&amp;"
 		case '\'':
 			// "&#39;" is shorter than "&apos;" and apos was not in HTML until HTML5.
-			esc = "&#39;"
+			esc = "&amp;#39;"
 		case '<':
 			esc = "&lt;"
 		case '>':
 			esc = "&gt;"
 		case '"':
 			// "&#34;" is shorter than "&quot;".
-			esc = "\\&#34;" // TODO: I am adding a slash here - needs to be factored out.
+			esc = "&amp;#34;" // TODO: I am adding a slash here - needs to be factored out.
 		case '\r':
 			esc = "&#13;"
 		case '\n':
 			esc = "&#10;"
-			panic("n")
 		default:
 			panic("unrecognized escape character")
 		}
