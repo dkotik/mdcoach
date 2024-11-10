4afe027d0d67c462e34fd6156692293787276387
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2024-11-10 18:44:37 +0200

    add negative aside test

--- mdextension/aside/parser_test.go
+++ mdextension/aside/parser_test.go
@@ -14,7 +14,8 @@ func TestAsideParserSuccess(t *testing.T) {
 	cases := [...][]byte{
 		[]byte(`::: title
 body
-:::`), []byte(`::::::::::::::::::::::::: title
+:::`),
+		[]byte(`::::::::::::::::::::::::: title
 body
 ::::::::::::::::::::::::::::::::: `),
 	}
@@ -47,5 +48,34 @@ body
 }
 
 func TestAsideParserFailure(t *testing.T) {
-	t.Skip("impl")
+	cases := [...][]byte{
+		[]byte(`:: title
+body
+:::`),
+		[]byte(`::::::::::::::::::::::::: title
+body
+::`),
+	}
+
+	p := parser.NewParser(parser.WithBlockParsers(util.Prioritized(aside.AsideBlockParser{}, 500)))
+
+	for i, cs := range cases {
+		tree := p.Parse(text.NewReader(cs))
+		if tree == nil {
+			t.Fatalf("could not parse case %d: parse tree is empty", i)
+		}
+
+		first := tree.FirstChild()
+		if first == nil {
+			return
+		}
+		if first.Kind() == aside.AsideBlockKind {
+			t.Fatal("parsed node is an aside block")
+		}
+
+		_, ok := first.(*aside.AsideBlockNode)
+		if ok {
+			t.Fatal("parsed node is an aside block")
+		}
+	}
 }
